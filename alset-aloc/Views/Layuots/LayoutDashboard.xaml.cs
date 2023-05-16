939e78931689d5a3cc6adccfed3708e4985216de
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-05-15 22:46:05 -0400

    feat: LayoutDashboard

---
+++ alset-aloc/Views/Layuots/LayoutDashboard.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace alset_aloc.Views.Layuots
+{
+    /// <summary>
+    /// Interação lógica para LayoutDashboard.xam
+    /// </summary>
+
+    [ContentProperty(nameof(Children))]  // Prior to C# 6.0, replace nameof(Children) with "Children"
+    public partial class LayoutDashboard : UserControl
+    {
+        public static readonly DependencyPropertyKey ChildrenProperty = DependencyProperty.RegisterReadOnly(
+            nameof(Children),  // Prior to C# 6.0, replace nameof(Children) with "Children"
+            typeof(UIElementCollection),
+            typeof(LayoutDashboard),
+            new PropertyMetadata());
+
+        public UIElementCollection Children
+        {
+            get { return (UIElementCollection)GetValue(ChildrenProperty.DependencyProperty); }
+            private set { SetValue(ChildrenProperty, value); }
+        }
+
+        public LayoutDashboard()
+        {
+            InitializeComponent();
+            Children = PART_Host.Children;
+        }
+    }
+}
