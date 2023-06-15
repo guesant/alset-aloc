aa1e5e97030c580591a08080e66fb127a291cb7a
Gabriel Rodrigues Antunes <gabrielrodantunes@gmail.com>
2023-06-15 08:58:27 -0400

    feat: CompraDAO

---
+++ alset-aloc/Models/Compra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alset_aloc.Models
+{
+    internal class Compra
+    {
+        public long Id { get; set; }
+
+        public DateTime? DataCompra { get; set; }
+
+        public string? NumeroNota { get; set; }
+
+        public long? ProdutoId { get; set; }
+
+        public long? FornecedorId { get; set; }
+    }
+}
